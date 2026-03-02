5f41e6294dc119f6d8b2d3fe1e0750b634acaa03
Christopher Jauregui <chris.jauran1@gmail.com>
2026-03-01 23:18:24 -0600

    Se agregan funciones para llenar informacion y logica de sorteo

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 let participantes=[];
+let restricciones={};
 
 
 //valida que el campo no esté vacio para poder continuar
@@ -10,6 +11,23 @@ btnAgregarCreador.addEventListener("click", agregarCreador);
 //inicia el llamado a la función de agregar participantes
 const btnAgregarParticipante=document.getElementById("btnAgregarParticipante");
 btnAgregarParticipante.addEventListener("click", agregarParticipante);
+
+
+//traemos el modal de las restricciones y le pasamos las opciones a los selects
+const modal3 = document.getElementById("exampleModal3");
+modal3.addEventListener("show.bs.modal", () => {
+    cargarSelects();
+});
+
+//cargar restricciones
+const btnAgregarRestricciones=document.getElementById("btnAgregarRestriccion");
+btnAgregarRestricciones.addEventListener("click", agregarRestricciones);
+
+
+//iniciamos el sorteo
+const btnComenzarSorteo=document.getElementById("btnIniciarSorteo");
+btnComenzarSorteo.addEventListener("click", comenzarSorteo);
+
 
 
 function agregarCreador(){
@@ -75,4 +93,145 @@ function actualizarLista(){
 function eliminarParticipante(index){
     participantes.splice(index,1);
     actualizarLista();
+}
+
+function cargarSelects(){
+    const selectBase= document.getElementById("personaBase");
+    const selectRestringida= document.getElementById("personaRestringida");
+
+    selectBase.innerHTML="";
+    selectRestringida.innerHTML="";
+
+    participantes.forEach(nombre=>{
+        selectBase.innerHTML+=`
+            <option value="${nombre}">${nombre}</option>    
+        `;
+        selectRestringida.innerHTML+=`
+            <option value="${nombre}">${nombre}</option>    
+        `;
+    })
+}
+
+function agregarRestricciones(){
+    const base = document.getElementById("personaBase").value;
+    const restringido = document.getElementById("personaRestringida").value;
+    
+    if(base===restringido){
+        alert("No puede restringirse a si mismo");
+        return;
+    }
+
+    if(!restricciones[base]){
+        restricciones[base]=[];
+    }
+
+    restricciones[base].push(restringido);
+
+
+    mostrarRestricciones();
+}
+
+function mostrarRestricciones(){
+    const lista=document.getElementById("listaRestricciones");
+    
+    lista.innerHTML="";
+
+    for(let persona in restricciones){
+        restricciones[persona].forEach(r=>{
+            lista.innerHTML+= `
+                <li class="list-group-item">
+                    ${persona} no puede sacar a ${r}
+                </li>
+            `;
+        });
+    }
+}
+
+
+
+//funcion que nos ayudará a validar que se cumplan las condiciones del sorteo
+function esValido(asignaciones){
+    //para cada persona de asignaciones comparamos
+    for (let persona in asignaciones){
+
+        //destino será la persona que se le asignó al momento de hacer el shuffle
+        let destino = asignaciones[persona];
+        
+        console.log("Validando:", persona, "->", destino);
+        console.log("Restricción:", restricciones[persona]);
+
+        //si la persona asignada es ella misma, retorna falso y no es valida la asignacion 
+        if(destino===persona) return false;
+        
+        //si la persona asignada es la que se encuentra restringida retorna falso y no es valida la asignación
+        if(restricciones[persona]?.includes(destino)){
+            return false;
+        }
+    }
+    
+    
+    //si ninguna de las dos condiciones previas se cumplen, quiere decir que la asignacion es correcta y retorna verdadero
+    return true;
+}
+
+//función que contiene la logica del sorteo
+function sorteo(){
+    let intentos= 0;
+    let maxIntentos=1000;
+
+    while (intentos<maxIntentos){
+
+        //copia del array que no altera el array original
+        let copia=[...participantes];
+
+        //algoritmo para asignacion aleatoria (shuffle)
+        for(let i=copia.length-1; i>0; i--){
+            let j=Math.floor(Math.random()*(i+1));
+            [copia[i], copia[j]]= [copia[j], copia[i]]
+        }
+
+        //array donde estarán las asignaciones 
+        let asignaciones={};
+
+        //para cada persona del array participantes se le asignará la persona que está en copia
+        participantes.forEach((persona, index)=>{
+            asignaciones[persona]=copia[index];
+        })
+
+        //se validan las asignacion con la funcion es valido
+        if(esValido(asignaciones)){
+            //depurar estos logs
+            console.log("Restricciones actuales:", restricciones);
+            console.log("ASIGNACIÓN FINAL:", asignaciones);
+            return asignaciones;//si todo se cumpla retorna las asignaciones creadas
+        }
+        //si no entra en el if, aumenta el contador
+        intentos++;
+    }
+    //si despues de hacer todos los intentos no nos arroja alguna combinación valida, regresamos un null
+    alert("No se pudo generar un sorteo válido.");
+    return null;
+
+}
+
+function comenzarSorteo(){
+    const resultado=sorteo();
+
+    if(resultado){
+        mostrarResultado(resultado);
+    }
+}
+
+function mostrarResultado(asignaciones){
+    const lista=document.getElementById("listaResultado");
+
+    lista.innerHTML="";
+
+    for(let persona in asignaciones){
+        lista.innerHTML+=`
+            <li class="list-group-item">
+                A ${persona} le toca: ${asignaciones[persona]}
+            </li>     
+        `;
+    }
 }
